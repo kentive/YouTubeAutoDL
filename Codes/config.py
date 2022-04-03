d41fed5269e45dadb10ab895bfd5e22d987903af
tsuchida kento <64092999+kenken-creater@users.noreply.github.com>
2022-04-03 19:05:09 +0900

    modify gitignore

--- Codes/config.py
+++
@@ -1,3 +0,0 @@
-KEY = 'AIzaSyBvIaGHbUFwZUb5t2t96QYu3ibc64P2-Vs'
-VIDEOS_DIR = '../Videos/'
-GIT_ACCESS_TOKEN = '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' # gitレポジトリのトークン、人に見せないやつ Expires on Sat, Jul 2 2022.
